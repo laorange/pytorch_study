69062e4bd38c2e4d5f03b6ce1bc864a4d36a26c8
paul-6666 <2625835752@qq.com>
2020-07-26 09:32:39 +0800

    2007260932

---
+++ study/study1.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+
+# np_data = np.arange(6).reshape((2, 3))
+# torch_data = torch.from_numpy(np_data)
+
+a = torch.Tensor([2, 3], [4, 8], [7, 9])
+
---
+++ study/study2_linear_model.py
@@ -0,0 +1,36 @@
+# 线性模型
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+
+def forward(x):
+    return x * w
+
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred - y) ** 2
+
+
+w_ls = []
+mse_ls = []
+
+for w in np.arange(0.0, 4.1, 0.1):
+    print('w=', w)
+    l_sum = 0
+    for x_val, y_val in zip(x_data, y_data):
+        y_pred_val = forward(x_val)
+        loss_val = loss(x_val, y_val)
+        l_sum += loss_val
+        print('\t', x_val, y_val, y_pred_val, loss_val)
+    print('MSE=', l_sum/3)
+    w_ls.append(w)
+    mse_ls.append(l_sum/3)
+
+plt.plot(w_ls, mse_ls)
+plt.xlabel('w')
+plt.ylabel('loss')
+plt.show()
