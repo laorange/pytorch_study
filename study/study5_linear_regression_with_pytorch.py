d96b5796faf03c1e355fbb5307d341ed492244af
paul-6666 <2625835752@qq.com>
2020-07-27 15:08:50 +0800

    2007271508

---
+++ study/study5_linear_regression_with_pytorch.py
@@ -0,0 +1,37 @@
+# 用pytorch实现线性回归
+import torch
+
+x_data = torch.Tensor([[1.0], [2.0], [3.0]])
+y_data = torch.Tensor([[2.0], [4.0], [6.0]])
+
+
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel, self).__init__()
+        self.linear = torch.nn.Linear(1, 1)
+
+    def forward(self, x):
+        y_pred = self.linear(x)
+        return y_pred
+
+
+model = LinearModel()
+
+criterion = torch.nn.MSELoss()
+optimiser = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(4000):
+    y_pred = model(x_data)
+    loss = criterion(y_pred, y_data)
+    print(epoch, loss.item())
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+
+print('w=', model.linear.weight.item())
+print('b=', model.linear.bias.item())
+
+x_test = torch.Tensor([[4.0]])
+y_test = model(x_test)
+print('y_pred=', y_test.data)
