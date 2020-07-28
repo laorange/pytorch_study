d4faf79887b1fa6e4e53876147ef10cb8ead9278
paul-6666 <2625835752@qq.com>
2020-07-28 17:40:16 +0800

    2007281740

--- study/study9_softmax_classifier.py
+++ study/study9_softmax_classifier.py
@@ -6,21 +6,26 @@ from torchvision import datasets
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 
+batch_size = 64
+
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))  # 均值 & 标准差, MNIST的经验值
 ])
 
-train_dataset = datasets.MNIST(root='../dataset/mnist/',
+train_dataset = datasets.MNIST(root='../dataset/',
                                train=True,
-                               download=True,
+                               download=False,
                                transform=transform)
 
-pass
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
 
-test_dataset = datasets.MNIST(root='../dataset/mnist/',
+test_dataset = datasets.MNIST(root='../dataset/',
                               train=True,
-                              download=True,
+                              download=False,
                               transform=transform)
 
+test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
+
 pass
+print('done')
