9a8894adbd9104302dacc50a4b24a49e589ea588
paul-6666 <2625835752@qq.com>
2020-07-28 22:04:08 +0800

    2007282204

--- study/study9_softmax_classifier.py
+++ study/study9_softmax_classifier.py
@@ -4,7 +4,7 @@ import torch
 from torchvision import transforms
 from torchvision import datasets
 from torch.utils.data import DataLoader
-import torch.nn.functional as F
+# import torch.nn.functional as F
 
 batch_size = 64
 
@@ -27,5 +27,63 @@ test_dataset = datasets.MNIST(root='../dataset/',
 
 test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 
-pass
-print('done')
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.l1 = torch.nn.Linear(784, 512)
+        self.l2 = torch.nn.Linear(512, 256)
+        self.l3 = torch.nn.Linear(256, 128)
+        self.l4 = torch.nn.Linear(128, 64)
+        self.l5 = torch.nn.Linear(64, 10)
+
+    def forward(self,x):
+        x = x.view(-1, 784)
+        x = torch.relu(self.l1(x))
+        x = torch.relu(self.l2(x))
+        x = torch.relu(self.l3(x))
+        x = torch.relu(self.l4(x))
+        return self.l5(x)
+
+
+model = Net()
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+
+        # forward + backward + update
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss
+        if batch_idx % 300 == 299:
+            print('第%d轮,训练次数:%d,loss:%.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+            running_loss = 0.0
+
+
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, label = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
+        print('Accuracy on test set: %d %%' % (100 * correct / total))
+
+
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
